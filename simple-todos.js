f8e395bf1c71645be1a1092f8bad975915da37e6
Xavier <xthom001@gmail.com>
2015-04-05 18:06:22 -0400

    Users can now create private and public posts

--- simple-todos.js
+++ simple-todos.js
@@ -1,38 +1,129 @@
 //simple-todos.js
+
 Tasks = new Mongo.Collection("tasks");
 
-if(Meteor.isClient){
-  // this code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      // show newest tasks first
-      return Tasks.find({}, {sort: {createdAt: -1}});
+if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
+    // this code only runs on the client
+    Template.body.helpers({
+        tasks: function() {
+            if (Session.get("hideCompleted")) {
+                // if hide completed is checked, filter tasks
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            } else {
+                // Otherwise return all of the tasks
+                // show newest tasks first
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            }
+        },
+        hideCompleted: function() {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function() {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }).count();
+        }
+    });
+    Template.body.events({
+        "submit .new-task": function(event) {
+            // this function is called when the new task form is submitted
+
+            var text = event.target.text.value;
+            // replaced task.insert
+            Meteor.call("addTasks", text);
+            // Clear form
+            event.target.text.value = "";
+
+            // prevent default form submit
+            return false;
+        },
+        "change .hide-completed input": function(event) {
+            Session.set("hideCompleted", event.target.checked);
+        }
+    });
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // set the checked property to the opposite of its current value
+            // replaces the task.update method 
+            Meteor.call("setChecked", this._id, ! this.checked);
+        },
+        "click .delete": function () {
+            Meteor.call("deleteTask", this._id);
+        },
+        "click .toggle-private": function () {
+            Meteor.call("setPrivate", this._id, ! this.private);
+        }
+    })
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+    Template.task.helpers({
+      isOwner: function () {
+        return this.owner === Meteor.userId();
+      }
+    });
+}
+Meteor.methods({
+  addTasks: function (text) {
+    // make sure the user is logged in before inserting a task
+    if ( ! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
     }
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meteor.userId()) {
+      // if the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meteor.userId()) {
+      // if task is private make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized"); 
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+    // make sure only the task owner can make a task private 
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update( taskId, { $set: { private: setToPrivate } });
+  }
+});
+if (Meteor.isServer) {
+  // only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // this function is called when the new task form is submitted
-
-      var text = event.target.text.value;
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.text.value = "";
-
-      // prevent default form submit
-      return false;
-    }
-  });
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
-  })
 }
